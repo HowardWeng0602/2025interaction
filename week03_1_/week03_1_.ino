04fcd292bccd4279c221dd73bc9f6c31660dfd55
HowardWeng0602 <145088372+HowardWeng0602@users.noreply.github.com>
2025-09-22 14:44:11 +0800

    week03

---
+++ week03_1_/week03_1_.ino
@@ -0,0 +1,14 @@
+// week03_1_
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8, OUTPUT);
+  tone(8, 400, 2000);
+  delay(2000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
